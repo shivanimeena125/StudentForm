833619fe63b00d94f8d731b9762f139d69d60508
shivanimeena125 <shivanimeena7454@gmail.com>
2025-06-26 11:01:02 +0530

    chnage program file

--- Program.cs
+++ Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Student}/{action=AddStudent}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
